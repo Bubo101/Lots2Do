83946971f69dd4ac7ace56c7a85f688a3b123fe1
Boden <boden.bradley@gmail.com>
2022-06-17 15:35:49 -0700

    Feature 5 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from projects.models import Project
 
 # Create your views here.
+class ProjectListView(ListView):
+    model = Project
+    template_name = "projects/list.html"
+    context_object_name = "projects_list"
